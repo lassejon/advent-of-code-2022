0d5d95bdd8b306fcfaeb0d353c121f4521c634a3
lassejon <lasss@live.dk>
2022-12-06 16:47:50 +0100

    mess

---
+++ Challenges/Day04.cs
@@ -0,0 +1,75 @@
+﻿namespace AdventOfCode.Challenges;
+
+public class Day04
+{
+    private InputReader? _inputReader;
+    private IEnumerable<IEnumerable<IEnumerable<int>>> _cleanedData = null!;
+
+    public static async Task<Day04> Initialize(InputReader? reader)
+    {
+        var thisObject = new Day04
+        {
+            _inputReader = reader
+        };
+
+        thisObject._cleanedData = await thisObject.CleanData();
+
+        return thisObject;
+    }
+
+    public int GetAnswerPart01()
+    {
+        return _cleanedData.Select(row =>
+        {
+            var first = row.First();
+            var second = row.Last();
+            return Contains(first, second) ? 1 : 0;
+        }).Sum();
+    }
+    
+    public int GetAnswerPart02()
+    {
+        return _cleanedData.Select(row =>
+        {
+            var first = row.First();
+            var second = row.Last();
+            return Overlaps(first, second) ? 1 : 0;
+        }).Sum();
+    }
+
+    private bool Contains(IEnumerable<int> first, IEnumerable<int> second)
+    {
+        return IsContained(first, second) || IsContained(second, first);
+    }
+
+    private static bool IsContained(IEnumerable<int> first, IEnumerable<int> second)
+    {
+        return first.First() >= second.First() && first.Last() <= second.Last();
+    }
+    
+    private bool Overlaps(IEnumerable<int> first, IEnumerable<int> second)
+    {
+        return IsOverlapping(first, second) || IsOverlapping(second, first);
+    }
+    
+    private static bool IsOverlapping(IEnumerable<int> first, IEnumerable<int> second)
+    {
+        return first.Last() >= second.First() && second.Last() >= first.First();
+    }
+
+    private async Task<IEnumerable<IEnumerable<IEnumerable<int>>>> CleanData()
+    {
+        var data = await GetData();
+        
+        return data.Select(r => r
+            .Split(",")
+            .Select(s => s
+                .Split("-")
+                .Select(l => int.Parse(l))));
+    }
+
+    private async Task<List<string?>> GetData()
+    {
+        return await _inputReader!.GetInput("2022/day/4/input");
+    }
+}
---
+++ Challenges/Day05.cs
@@ -0,0 +1,107 @@
+﻿namespace AdventOfCode.Challenges;
+
+public class Day05
+{
+    private InputReader? _inputReader;
+    private List<string?> _data = null!;
+    private List<List<char>> _stacks = null!;
+    private List<IEnumerable<char>> _instructions = null!;
+    private int _skipLength;
+
+    public static async Task<Day05> Initialize(InputReader? reader)
+    {
+        var thisObject = new Day05
+        {
+            _inputReader = reader
+        };
+
+        thisObject._data = await thisObject.GetData();
+        thisObject._stacks = await thisObject.GetStacks();
+        thisObject._skipLength = thisObject.GetSkipLength();
+        thisObject._instructions = thisObject.GetInstructions();
+
+        var x = thisObject.GetMovedStacks();
+
+        return thisObject;
+    }
+
+    private IEnumerable<IEnumerable<char>> GetMovedStacks()
+    {
+        _instructions.ForEach(i =>
+        {
+            var pilesToMove = int.Parse(i.ElementAt(0).ToString());
+            var moveFrom = int.Parse(i.ElementAt(1).ToString());
+            var moveTo = int.Parse(i.ElementAt(2).ToString());
+
+            var range = new List<char>();
+            while (pilesToMove > 0)
+            {
+                var last = _stacks[moveFrom].Count - 1;
+                range.Add(_stacks[moveFrom][last]);
+                _stacks[moveFrom].RemoveAt(last);
+
+                pilesToMove--;
+            }
+            
+            _stacks[moveTo].AddRange(range);
+        });
+
+        return _stacks;
+    }
+
+    private List<IEnumerable<char>> GetInstructions()
+    {
+        var x = _data.Skip(_skipLength).Select(row => row.Where(char.IsNumber));
+        return x.ToList();
+    }
+    
+    private async Task<List<List<char>>> GetStacks()
+    {
+        var matrixLength = _data.First().Length;
+
+        var matixList = _data.Where(row => row.Length == matrixLength);
+
+        var matrixArray = matixList.Select(row => row.ToCharArray());
+
+        var matrix = RotateMatrixCounterClockwise(matrixArray);
+        var stacks = matrix
+            .Where(stack => char.IsNumber(stack.Last()))
+            .Select(stack => stack.Reverse().Skip(1).Where(char.IsLetter).ToList())
+            .Reverse();
+        
+        return stacks.ToList();
+    }
+
+    private int GetSkipLength()
+    {
+        return _stacks.Max(x => x.Count()) + 2;
+    }
+    
+    private static IEnumerable<char[]> RotateMatrixCounterClockwise(IEnumerable<char[]> oldMatrix)
+    {
+        var newMatrix = new char[oldMatrix.First().Length][];
+
+        for (var i = 0; i < oldMatrix.First().Length; i++)
+        {
+            newMatrix[i] = new char[oldMatrix.Count()];
+        }
+        
+        var newRow = 0;
+        for (var oldColumn = oldMatrix.First().Length - 1; oldColumn >= 0; oldColumn--)
+        {
+            var newColumn = 0;
+            for (var oldRow = 0; oldRow < oldMatrix.Count(); oldRow++)
+            {
+                newMatrix[newRow][newColumn] = oldMatrix.ElementAt(oldRow)[oldColumn];
+                newColumn++;
+            }
+            newRow++;
+        }
+        return newMatrix;
+    }
+
+    private async Task<List<string?>> GetData()
+    {
+        return await _inputReader!.GetInput("2022/day/5/input");
+    }
+}
